e8da7a25292aa189388930c334b64913ded6ae92
jglez <jglez.cs@gmail.com>
2021-03-16 19:10:37 -0500

    Met MVP for SQL Queries

--- queries.sql
+++ queries.sql
@@ -22,8 +22,12 @@ SELECT * FROM customers
 WHERE (city = 'London') or (city = 'Madrid') or (country = 'Brazil')
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO customers (customername, contactname, address, city, postalcode, country)
+values('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE customers SET postalcode = '11122'
+WHERE customerid = 92
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
