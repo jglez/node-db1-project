121d818cc7f0bd7a4ef310bca06b0a554fdf8700
jglez <jglez.cs@gmail.com>
2021-03-17 21:01:42 -0500

    Converted SQL getById helper function to proper Knex syntax

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -3,20 +3,29 @@ const { default: knex } = require('knex')
 const db = require('../../data/db-config.js')
 
 const getAll = () => {
+  // Proper Knex syntax
   return db.select().table('accounts')
-  // return db('accounts') // knex short hand
+
+  // return db('accounts') // proper knex short hand
   // return db.raw(`SELECT * FROM accounts`) // raw SQL === BAD!
 }
 
 const getById = id => {
+  // Proper Knex syntax
+  return db('accounts').where({ id })
 
-  return db.raw(`
-    SELECT * FROM accounts
-    WHERE id = ${id}
-  `)
+  // return db.raw(`
+  //   SELECT * FROM accounts ---> this is better raw SQL
+  //   WHERE id = ?`, [id])
+
+  // return db.raw(`
+  //   SELECT * FROM accounts 
+  //   WHERE id = ${id} // ---> vulnerable to SQL injection attacks
+  // `) // NEVER interpolate client data directly
+  // SUPER BAD
 }
 
-const create = ({ name, budget }) => {
+const create = async account => {
 
 }
 
